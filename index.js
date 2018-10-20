84d609a9d0626059b140effb6ffff2fdc3d55287
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-10-20 14:15:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const discord = require("discord.js");
 const bot = new discord.Client();
 const fs = require ("fs");
 token = process.env.token
-> = prefix
 
 bot.commands = new discord.Collection
 fs.readdir("./commands/", (err, files) => {
@@ -32,7 +31,7 @@ bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
-    let prefix = prefix;
+    let prefix = >;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
